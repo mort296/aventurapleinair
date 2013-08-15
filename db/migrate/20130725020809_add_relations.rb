145565f29cebe29708d3896016106d9e6dc63c82
Maxime <cloutier_16@hotmail.com>
2013-08-15 15:13:03 -0400

    modified links table relation

--- db/migrate/20130725020809_add_relations.rb
+++ db/migrate/20130725020809_add_relations.rb
@@ -8,22 +8,21 @@ class AddRelations < ActiveRecord::Migration
       t.belongs_to :activity
       t.belongs_to :event
     end
+    create_table :links do |t|
+      t.string :url
+        t.references :linkable, polymorphic: true
+        t.timestamps
+    end
 
-    add_column :links, :linkable_id, :integer
-    add_column :links, :linkable_type, :string
-
-    remove_column :links, :type
     add_column :activities, :season_id, :integer
     add_column :events, :season_id, :integer
     add_column :administrative_regions, :location_id, :integer
     add_column :activity_categories, :activity_id, :integer
   end
   def self.down
+    drop_table :links
   	drop_table :activity_location
   	drop_table :activity_event
-  	add_column :links, :type, :string
-  	remove_column :links, :linkable_id
-  	remove_column :links, :linkable_type
     remove_column :activities, :season
     remove_column :events, :season
     remove_column :administrative_regions, :location_id
