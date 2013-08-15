853981c49b8ca250324a580c5bad3440a3b60901
Maxime <cloutier_16@hotmail.com>
2013-08-15 16:06:58 -0400

    added a login for the prob ENV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :check_access if Rails.env == "production"
+  
+  private
+  def check_access
+    authenticate_or_request_with_http_basic do |user_name, password|
+	    # Change these to username and password required
+	    user_name == "aventura" && password == "pleinair"
+  	end
+  end
 end
