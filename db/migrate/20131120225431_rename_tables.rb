5c440149c7803831b588b405c7bae2ddc91651d5
Alexia <usagerx@MacBook-Air-de-Usager.local>
2013-11-20 23:25:47 -0500

    fixed the migrations

--- db/migrate/20131120225431_rename_tables.rb
+++
@@ -1,7 +0,0 @@
-class RenameTables < ActiveRecord::Migration
-  def change
-  	rename_table :top_activities, :top_activity
-  	rename_table :top_events, :top_event
-  	rename_table :top_locations, :top_location
-  end
-end
