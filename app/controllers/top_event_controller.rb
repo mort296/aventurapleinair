f5af246af952bb8160380f42a52761ef502999e7
Maxime <cloutier_16@hotmail.com>
2013-12-02 00:30:35 -0500

    updated the top 10s pages querie

--- app/controllers/top_event_controller.rb
+++ app/controllers/top_event_controller.rb
@@ -1,6 +1,6 @@
 class TopEventController < ApplicationController
 	def index
 		@text = TopEvent.first()
-		@topEvents = Event.all.joins(:event_rating).order('event_ratings.rate DESC').limit(10)
+		@topEvents = Event.all.includes(:event_rating).order('event_ratings.rate DESC').limit(10)
 	end
 end
