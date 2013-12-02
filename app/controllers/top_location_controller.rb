6032693543e2051f91585125b3a61ca1f9142663
Maxime <cloutier_16@hotmail.com>
2013-12-02 00:40:42 -0500

    updated the top 10s pages querie

--- app/controllers/top_location_controller.rb
+++ app/controllers/top_location_controller.rb
@@ -1,6 +1,6 @@
 class TopLocationController < ApplicationController
 	def index
 		@text = TopLocation.first()
-		@topLocations = Location.all.includes(:location_rating).order('location_ratings.rate DESC').limit(10)
+		@topLocations = Location.all.joins(:location_rating).order('location_ratings.rate DESC').limit(10)
 	end
 end
