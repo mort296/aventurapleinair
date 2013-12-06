29b808a0f84a4caddaa6e2528680e9fd04819056
Maxime <cloutier_16@hotmail.com>
2013-12-05 22:51:46 -0500

    fixed prod bug

--- app/admin/about_pages.rb
+++ app/admin/about_pages.rb
@@ -4,12 +4,4 @@ ActiveAdmin.register AboutPage do
 
 	actions  :index, :edit, :update
 
-	controller do
-		def new
-			@about_page = AboutPage.new
-			ADDITIONAL_LOCALES.each do |lang|
-				@about_page.translations.find_or_initialize_by_locale(lang[0]) unless lang[0] == :fr
-			end
-		end
-	end	
 end
