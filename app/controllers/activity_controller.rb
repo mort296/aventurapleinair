8b5593421d9b0318aaf91b99487c479e2ca5cff7
Maxime <cloutier_16@hotmail.com>
2014-02-09 20:43:57 -0500

    fixed activities prob

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -2,9 +2,10 @@ class ActivityController < ApplicationController
   def show
   	@activity = Activity.find(params[:id])
     
-    
-    @activity.video_link = @activity.video_link.gsub('https://', '')
-    @activity.video_link = @activity.video_link.gsub('http://', '')
+    if @activity.video_link?
+      @activity.video_link = @activity.video_link.gsub('https://', '')
+      @activity.video_link = @activity.video_link.gsub('http://', '')
+    end
     
     @video = Conred::Video.new(
       video_url: @activity.video_link, 
