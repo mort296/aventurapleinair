92a975d31708a4f89a34e1fc9dd8aa30825a04d3
Maxime <cloutier_16@hotmail.com>
2014-02-11 17:54:02 -0500

    removed http login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :check_access if Rails.env == "production"
-
   before_filter :get_footer_header
 
   before_filter :set_locale
@@ -20,12 +18,5 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = session[:locale] || I18n.default_locale
     session[:locale] = I18n.locale
-  end
-
-  def check_access
-    authenticate_or_request_with_http_basic do |user_name, password|
-	    # Change these to username and password required
-	    user_name == "aventura" && password == "pleinair"
-  	end
   end
 end
