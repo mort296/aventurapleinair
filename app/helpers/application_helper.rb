e41da893daa6e773b3cd2cfbdb66b9b34e2e703b
Maxime <cloutier_16@hotmail.com>
2014-03-25 17:20:15 -0400

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
+  def meta_desc(page_description)
+    content_for(:meta_desc) { page_description }
+  end
+
   def rating_class(number)
     ratingNumber = {'1' => 'one', '2' => 'two', '3' => 'three', '4' => 'four', '5' => 'five'}
     ratingNumber[number.to_s]
