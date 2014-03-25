520f9a7e1eda6c5cd72a315955fbbc9ba6914949
Maxime <cloutier_16@hotmail.com>
2014-03-24 20:38:11 -0400

    .

--- app/admin/political_reviews.rb
+++ app/admin/political_reviews.rb
@@ -4,5 +4,12 @@ ActiveAdmin.register PoliticalReview do
 
 	actions  :index, :edit, :update
 
-	
+	controller do
+		def new
+			@political_review = PoliticalReview.new
+			ADDITIONAL_LOCALES.each do |lang|
+				@political_review.translations.find_or_initialize_by_locale(lang[0]) unless lang[0] == :fr
+			end
+		end
+	end
 end
