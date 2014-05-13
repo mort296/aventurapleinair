8740f6a9a25d2c9c6cfd8da2fe1fb9efbbd11734
Maxime <cloutier_16@hotmail.com>
2014-05-13 19:17:28 -0400

    .

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,7 +4,7 @@ class Activity < ActiveRecord::Base
 
   acts_as_commentable
   validates_presence_of :name
-  validates_presence_of :description, :particularity, :learn, :equipment, :season, :activity_category, :locations, :image, :if => :online?
+  validates_presence_of :description, :season, :activity_category, :locations, :image, :if => :online?
   
 	translates :name, :federation, :particularity, :description, :other_infos, :learn, :equipment, :useful_links, :image_caption_activity, :image_caption_location, :image_caption_text, :video_link, :fallbacks_for_empty_translations => true
   accepts_nested_attributes_for :translations
